328b101fa823c3dcc07034a0acadbec836a13dbb
Pawanjot Randhawa <pawanjot2004@gmail.com>
2025-08-13 13:44:21 -0400

    refactored code

--- components/login.tsx
+++ components/login.tsx
@@ -30,7 +30,7 @@ export default function Login() {
           rounded-lg text-xl px-20 py-5 text-center me-2 mb-2 
           dark:border-blue-500 dark:text-blue-500 dark:hover:text-white 
           dark:hover:bg-blue-500 dark:focus:ring-blue-800
-          dark:hover:cursor-pointer"
+          dark:hover:cursor-pointer cursor-pointer"
           onClick={logGoogleUser}
         >
           Login
